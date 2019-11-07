e1c11c3ccdf77943874342b9d2ff0005d1b7855d
jonasL96 <47625852+jonasL96@users.noreply.github.com>
2019-11-07 08:25:25 +0200

    Learning algorithm added

--- ld3.py
+++ ld3.py
@@ -2,11 +2,14 @@ import random
 import numpy
 import array
 import math
+import time
 
 x = [0.0] * 20
 y = [0.0] * 20
 actual = [0.0] * 20
 e = [0.0] * 20
+F1 = [0.0] * 20
+F2 = [0.0] * 20
 i = 0
 while i < 20:
     #Generating inputs
@@ -24,19 +27,53 @@ while i < 20:
 #c and r parameters and initial w1,w2,w0
 c1 = 0.35
 c2 = 0.5
-r1 = 2
-r2 = 1.87
+r1 = 0.5
+r2 = 0.4
 w1 = random.uniform(0,1)
 w2 = random.uniform(0,1)
 w0 = random.uniform(0,1)
 #Calculating RBF
 i = 0
 while i < 20:
-    F1 = math.exp(((-1)*(x[i]-c1)**2)/(2*r1**2))
-    F2 = math.exp(((-1)*(x[i]-c2)**2)/(2*r2**2))
-    actual[i] = F1*w1+F2*w2+w0
+    F1[i] = math.exp(((-1)*(x[i]-c1)**2)/(2*r1**2))
+    F2[i] = math.exp(((-1)*(x[i]-c2)**2)/(2*r2**2))
+    actual[i] = F1[i]*w1+F2[i]*w2+w0
     #error
     e[i] = y[i] - actual[i]
     i = i + 1
 
 e_total = sum(e)
+eta = 0.1
+iterations = 1000
+executions = 0
+i = 0
+k = 0
+while e_total != 0:
+    executions = executions + 1
+    while k < 20:
+        #weight update
+        w1 = w1 + eta*e_total*F1[k]
+        w2 = w2 + eta*e_total*F2[k]
+        w0 = w0 + eta*e_total
+        e_total = 0
+        #trying updated weights
+        while i < 20:
+            F1[i] = math.exp(((-1) * (x[i] - c1) ** 2) / (2 * r1 ** 2))
+            F2[i] = math.exp(((-1) * (x[i] - c2) ** 2) / (2 * r2 ** 2))
+            actual[i] = F1[i] * w1 + F2[i] * w2 + w0
+            e[i] = y[i] - actual[i]
+            e_total = e_total + e[i]
+            i = i + 1
+        if i == 20:
+            i = 0
+        e_total = round(e_total,2)
+        k = k + 1
+    if k == 20:
+        k = 0
+    if e_total == 0:
+        print("Learning algorithm succeeded in eliminating errors. Amount of executions: ",executions)
+        break
+    if executions == iterations:
+        print("Maximum amount of executions reached")
+        break
+
